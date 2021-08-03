fe3c90a3b06f54b364a9c5ed7e4db78efba679dc
Pavan_Chandu <65697605+sasipavanchandu@users.noreply.github.com>
2021-08-03 13:53:30 +0530

    changes

---
+++ script.js
@@ -0,0 +1,18 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML =  interest ;
+}
